d91b7d37afd139662265e3dd7bcf7e034123e2c8
mdrokz <mohammadmunshi@gmail.com>
2022-06-17 14:33:08 +0530

    get manga_name from args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-
 const BASE_URL: &str = "https://mangasee123.com/read-online";
 
 #[tokio::main]
-async fn main() {
-    let args = std::env::args();
+async fn main() -> Result<(), String> {
+    let mut args = std::env::args();
+
+    args.next();
+
+    let manga_name = args
+        .next()
+        .map_or(Err("Argument manga name is missing"), |f| Ok(f))?;
+
+    println!("{}",manga_name);
+
+    Ok(())
 }
