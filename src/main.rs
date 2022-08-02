3819a27bb79784ee58fc4688821223c8879d00b6
mdrokz <mohammadmunshi@gmail.com>
2022-08-03 00:51:17 +0530

    change logic to record checkpoints & resume

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-// #![feature(async_closure)]
+#![feature(async_closure)]
 
-use std::{collections::VecDeque, io::Write, time::Duration};
+use std::fs::OpenOptions;
+use std::future::Future;
+use std::io::Read;
+use std::path::Path;
+use std::{io::Write, time::Duration};
 
 use async_once::AsyncOnce;
 use fantoccini::{Client, ClientBuilder, Locator};
-use std::fs::{create_dir_all,read_dir, File};
-
-// use scraper::{Html, Selector};
+use std::fs::{create_dir_all, read_dir, File};
 
 #[macro_use]
 extern crate lazy_static;
 
 const BASE_URL: &str = "https://mangasee123.com";
+
+const CHECKPOINT_FILE: &str = "checkpoint.txt";
 
 #[macro_use]
 mod json;
@@ -64,6 +68,23 @@ impl Into<String> for MangaType {
     }
 }
 
+async fn async_filter<'a: 'b, 'b, T: 'a + 'b + Clone, F: Future<Output = bool>>(
+    a: &'a Vec<T>,
+    p: impl Fn(&'b T) -> F,
+) -> Vec<T>
+// where
+//     P: Fn(&T) -> Pin<Box<dyn Future<Output = bool>>>,
+{
+    let mut new = vec![];
+
+    for x in a.iter() {
+        if p(x).await {
+            new.push(x.clone());
+        }
+    }
+    return new;
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = std::env::args();
@@ -85,54 +106,127 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         manga_name
     );
 
+    let mut checkpoint_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(false)
+        .create(true)
+        .open(Path::new(&manga_dir).join(CHECKPOINT_FILE))?;
+
+    let checkpoint_text = {
+        let mut buf = String::new();
+        let _ = checkpoint_file.read_to_string(&mut buf).map_or(0, |s| s);
+        buf
+    };
+
+    let mut checkpoint_split = checkpoint_text.split("\n");
+
+    let checkpoint_link = checkpoint_split.next().map_or("", |f| f);
+
     if let Err(_) = read_dir(&manga_dir) {
         create_dir_all(&manga_dir)?;
     }
 
     let c = CLIENT.get().await;
 
-    c.goto(&format!("{}/manga/{}", BASE_URL, manga_name))
-        .await?;
+    let mut chapter_len = if checkpoint_text.is_empty() {
+        (0, 0)
+    } else {
+        let mut checkpoint = checkpoint_split
+            .next()
+            .expect("failed to get next element for checkpoint")
+            .split(",");
 
+        (
+            checkpoint.next().map_or(0, |f| {
+                f.parse::<usize>().expect("failed to parse chapter count")
+            }),
+            checkpoint.next().map_or(0, |f| {
+                f.parse::<usize>().expect("failed to parse chapter count")
+            }),
+        )
+    };
     let mut chapters = vec![];
     let mut index = 1;
 
-    let show_all_chapters = c.find(Locator::Css(".ShowAllChapters")).await?;
+    if checkpoint_text.is_empty() {
+        c.goto(&format!("{}/manga/{}", BASE_URL, manga_name))
+            .await?;
 
-    show_all_chapters.click().await?;
+        let show_all_chapters = c.find(Locator::Css(".ShowAllChapters")).await?;
 
-    let chapter_link_elements = c.find_all(Locator::Css(".top-10>a")).await?;
+        show_all_chapters.click().await?;
 
-    let mut chapter_links = VecDeque::with_capacity(chapter_link_elements.len());
+        let chapter_link_elements = c
+            .find_all(Locator::Css(r#".top-10>a[class*="ChapterLink"]"#))
+            .await?;
 
-    for chapter_link in chapter_link_elements {
-        chapter_links.push_front(tokio::spawn(async move {
-            let link = chapter_link
+        chapter_len.1 = chapter_link_elements.len();
+
+        c.goto(
+            &chapter_link_elements
+                .last()
+                .expect("failed to get 1st chapter link")
                 .attr("href")
-                .await
-                .map_or(String::new(), |f| f.expect("failed to extract href"));
-            link
-        }));
+                .await?
+                .map_or(String::new(), |f| f),
+        )
+        .await?;
+        // for chapter_link in chapter_link_elements {
+        //     chapter_links.push_front(tokio::spawn(async move {
+        //         let link = chapter_link
+        //             .attr("href")
+        //             .await
+        //             .map_or(String::new(), |f| f.expect("failed to extract href"));
+        //         link
+        //     }));
+        // }
+    } else {
+        c.goto(checkpoint_link).await?;
     }
 
-    for chapter in chapter_links {
-        let href = chapter.await?;
+    for chapter in chapter_len.0..chapter_len.1 {
+        let url = c.current_url().await?;
 
-        if !href.contains("read-online") {
-            continue;
-        }
+        checkpoint_file
+            .write_all(format!("{}\n{},{}\n", url, chapter, chapter_len.1).as_bytes())?;
 
-        tokio::spawn(async move {
-            c.goto(&format!("{}{}", BASE_URL, href))
-                .await
-                .expect("failed to navigate")
-        });
+        let btn_elements = &c.find_all(Locator::Css(".nav-link")).await?;
 
-        tokio::time::sleep(Duration::from_millis(5000)).await;
+        // let filtered_btn = async_filter(&btn_elements.clone(), async move |e| {
+        //     let t = e.attr("ng-click").await.map_or(String::new(), |f| if let Some(f) = f {
+        //         f
+        //     } else {
+        //         String::new()
+        //     });
 
+        //     t.contains("vm.Next")
+        // })
+        // .await;
+
+        let next_btn = btn_elements[4].clone();
+
+        // let next_btn = filtered_btn.first().ok_or("failed to get first element")?;
+
+        // let href = chapter.await?;
+
+        // if !href.contains("read-online") {
+        //     continue;
+        // }
+        // tokio::spawn(async move {
+        //     c.goto(&format!("{}{}", BASE_URL, href))
+        //         .await
+        //         .expect("failed to navigate")
+        // });
         let button = &c.find_all(Locator::Css(".Column>.btn-sm")).await?[3];
 
-        button.click().await?;
+        let btn_text = button.text().await?;
+
+        if btn_text.contains("Long Strip") {
+            button.click().await?;
+        }
+
+        tokio::time::sleep(Duration::from_millis(5000)).await;
 
         let images = c.find_all(Locator::Css(".ng-scope>div>img")).await?;
 
@@ -164,7 +258,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         image_links.push(link);
                     }
                     Err(_) => {
-                        println!("failed to download image");
+                        println!("failed to screenshot image {}", link);
                     }
                 };
             }
@@ -176,6 +270,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
 
         index += 1;
+
+        // c.execute(r#"[...document.getElementsByClassName("nav-link")].filter(x => x.innerText.includes("Chapter"))[1].click()"#, vec![]).await?;
+
+        next_btn.click().await?;
     }
 
     let chapters: Vec<String> = chapters.iter().map(|x| x.serialize()).collect();
